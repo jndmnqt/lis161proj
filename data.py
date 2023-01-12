be0d0cc16625ab98f991bec790bc33ff586d61c5
me <jmtimbol@up.edu.ph>
2023-01-12 18:19:00 +0800

    added quantity (amount)

--- data.py
+++ data.py
@@ -4,12 +4,14 @@ plants = {
             'name': 'aloe vera',
             'price': 700,
             'note': 'Oral ingestion of aloe vera is potentially toxic.',
+            'amount': 30,
             'description': 'A herb with succulent leaves that are arranged in a rosette. They have sharp, pinkish spines.',
             'url': 'https://www.thespruce.com/thmb/bW_aNpJmmnM0a_ySCdsYanFuAEM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/_DSC58741-476b7f7ef66d44ba8ce33cbc0d7b8dd4.jpg'
         },
         {
             'name': 'jade plant',
             'price': 650,
+            'amount': 24,
             'note': 'It lives for a long time, taking on the appearance of a miniature tree as it ages.',
             'description': 'The jade plant has its features thick, with woody stems and glossy green, fleshy, oblong leaves up to two inches long.',
             'url': 'https://www.gardeningknowhow.com/wp-content/uploads/2010/01/jade-plant-1.jpg'
@@ -17,6 +19,7 @@ plants = {
          {
             'name': 'sunburst',
             'price': 650,
+            'amount': 56,
             'note': 'Sunburst is monocarpic, meaning that after it flowers, the main plant will die.',
             'description': 'Sunburst leaves are white and green, with pink edges that turn red in full sun.',
             'url': 'https://cdn.shopify.com/s/files/1/0259/6015/3122/products/Aeonium_SunburstCristata.png'
@@ -24,6 +27,7 @@ plants = {
          {
             'name': 'bunny ears cactus',
             'price': 700,
+            'amount': 10,
             'note': 'It is dotted with fine spines called glochids that will get into your skin and make you itch.',
             'description': 'Cactus that has pads resembling rabbit ears that look soft enough to stroke.',
             'url': 'https://images.ctfassets.net/3s5io6mnxfqz/4sMpJVlBgtCyxfhm2IX6mn/91b216da9da9138542fe9c8f4767b7bb/AdobeStock_100596658_2.jpeg'
@@ -33,6 +37,7 @@ plants = {
         {
             'name': 'peace lily',
             'price': 550,
+            'amount': 14,
             'note': 'The plant does not need large amounts of light or water to survive.',
             'description': 'Peace lily has glossy, dark green oval leaves that narrow to a point. It periodically produce lightly fragrant white flowers that resemble calla lilies.',
             'url': 'https://www.miraclegro.com/sites/g/files/oydgjc111/files/styles/scotts_asset_image_720_440/public/asset_images/dreamstimemaximum_70276461_1080x608_v2.jpg'
@@ -40,6 +45,7 @@ plants = {
         {
             'name': 'African violet',
             'price': 600,
+            'amount': 34,
             'note': 'Spilling cold water on African violet leaves causes discoloration.',
             'description': 'The leaves are rounded to oval and finely hairy with a fleshy texture. Its flowers has five-lobed velvety petals growing in clusters.',
             'url': 'https://www.allaboutgardening.com/wp-content/uploads/2022/05/african-violets-blooming-indoors-1200x667.jpeg'
@@ -47,6 +53,7 @@ plants = {
         {
             'name': 'anthurium',
             'price': 600,
+            'amount': 12,
             'note': 'Anthurium is a natural air purifier.',
             'description': 'Anthurium has glossy, green heart-shaped leaves topped by heart-shaped pink, red, or white long-lasting blooms.',
             'url': 'https://www.petalrepublic.com/wp-content/uploads/2022/05/9-Amazing-Uses-and-Benefits-of-Anthurium-Plants.jpeg'
@@ -56,6 +63,7 @@ plants = {
         {
             'name': 'Japanese maple',
             'price': 3000,
+            'amount': 0,
             'note': 'In summer, they take on an intense green color, but when autumn arrives, the color fades to intense garnet tones.',
             'description': 'It is famous for its spectacular sprouts, with an intense carmine color. Its leaves are palm-like, with slightly serrated edges.',
             'url': 'https://i.pinimg.com/originals/9c/fe/be/9cfebe025619fd608cedae15bb2b3c00.jpg'
@@ -64,12 +72,14 @@ plants = {
             'name': 'pomegranate',
             'price': 2500,
             'note': '',
+            'amount': 3,
             'description': 'Has a thick trunk with interesting bark as well as eye-catching flowers that bear fruit.',
             'url': 'https://www.bonsaiempire.com/images/carrousel/pomegranate-bonsai-1-tree-3.jpg'
         },
         {
             'name': 'azalea',
             'price': 3000,
+            'amount': 4,
             'note': 'Its shrubs have shallow roots that can be easily damaged if grown in hot, sunny sites.',
             'description': 'This particular plant is very hardy and produces magnificent flowers that bloom brilliantly with radiant colors in late spring.',
             'url': 'https://www.bonsaiempire.com/images/carrousel/Azalea06-azalea-flowers.jpg'
