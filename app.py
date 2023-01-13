ad98f34afe049052a8091239d1ad95cc7aed9f54
me <jmtimbol@up.edu.ph>
2023-01-13 12:24:10 +0800

    changed something

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ def index():
 @app.route('/plantae/<plant_type>')
 def plantae(plant_type):
     plant_list = plants[plant_type]
-    return render_template('plantae.html',plant_type_template=plant_type,plants_template=plant_list)
+    return render_template('plantae.html',plant_category=plant_type,plants_list=plant_list)
 
 @app.route('/plantae/<plant_type>/<int:plant_id>')
 def plant(plant_type,plant_id):
+    plant_list = plants[plant_type]
     plant_details=plants[plant_type][plant_id]
-    return render_template('plant.html',plant_details=plant_details)
+    return render_template('plant.html',plant_details=plant_details,plant_category=plant_type,plants_list=plant_list)
  
 if __name__ == "__main__":
     app.run(debug=True)
